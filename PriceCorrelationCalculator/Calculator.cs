be6432aed10f82c9631ca4b2310d3992bf9d67fc
wshuts <shutswilliam@gmail.com>
2020-05-12 20:12:01 -0500

    adding PriceCorrelationCalculator library

---
+++ PriceCorrelationCalculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace PriceCorrelationCalculator
+{
+    public class Calculator
+    {
+        public void CalculateCorrelation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

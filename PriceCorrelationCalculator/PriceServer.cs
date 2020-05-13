4f29ed26d7cb30d65e87e23088bfbca81748d76a
wshuts <shutswilliam@gmail.com>
2020-05-13 01:18:23 -0500

    creating FundTable, implementing ParseFundTable

--- PriceCorrelationCalculator/PriceServer.cs
+++ PriceCorrelationCalculator/PriceServer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Security.Authentication;
@@ -18,7 +17,30 @@ namespace PriceCorrelationCalculator
 
         private void ParseFundTable(string responseFromServer)
         {
-            throw new System.NotImplementedException();
+            IList fundTableLines=new ArrayList();
+
+            var parsedStrings = responseFromServer.Split('\n');
+
+            fundTableLines.Clear();
+            foreach(var line in parsedStrings)
+            {
+                if(line.IndexOf("</option>", StringComparison.Ordinal)!=-1)
+                {
+                    fundTableLines.Add(line);
+                }
+            }
+			
+            for(var count=0;count<5;count++)
+            {
+                if(fundTableLines.Count>0)fundTableLines.RemoveAt(0);
+            }
+			
+            FundTable.Clear();
+            foreach(string line in fundTableLines)
+            {
+                var chunks = line.Split('\"','>','<');
+                FundTable.Add(chunks[4],chunks[2]);
+            }
         }
 
         private static string ReadFromWeb(string requestUri)
@@ -51,6 +73,6 @@ namespace PriceCorrelationCalculator
             return requestUri;
         }
 
-        public IEnumerable<DictionaryEntry> FundTable { get; set; }
+        public IDictionary FundTable { get; set; } = new SortedList();
     }
 }

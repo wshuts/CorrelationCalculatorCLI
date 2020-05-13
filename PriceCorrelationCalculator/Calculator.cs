ade6420deae0cbbbdf40742f9f262fb37c842c39
wshuts <shutswilliam@gmail.com>
2020-05-13 00:23:13 -0500

    InitializeFundTable

--- PriceCorrelationCalculator/Calculator.cs
+++ PriceCorrelationCalculator/Calculator.cs
@@ -1,4 +1,6 @@
-﻿namespace PriceCorrelationCalculator
+﻿using System.Collections;
+
+namespace PriceCorrelationCalculator
 {
     public class Calculator
     {
@@ -6,5 +8,27 @@
         {
             throw new System.NotImplementedException();
         }
+
+        public void InitializeFundTable()
+        {
+            PriceServer.GetFundTable();
+
+            FundTable.Clear();
+            foreach (var dictionaryEntry in PriceServer.FundTable)
+            {
+                FundTable.Add(dictionaryEntry.Key, dictionaryEntry.Value);
+            }
+
+            RemoveMoneyMarketFunds();
+        }
+
+        private void RemoveMoneyMarketFunds()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PriceServer PriceServer { get; set; }
+
+        public IDictionary FundTable { get; set; }
     }
 }

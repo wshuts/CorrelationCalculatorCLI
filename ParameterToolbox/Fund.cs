2fb784c38cd1815a597da4d11045fc594615ed10
wshuts <shutswilliam@gmail.com>
2020-05-20 02:20:14 -0500

    more Fund properties

--- ParameterToolbox/Fund.cs
+++ ParameterToolbox/Fund.cs
@@ -1,9 +1,13 @@
-﻿namespace ParameterToolbox
+﻿using System.Collections;
+
+namespace ParameterToolbox
 {
     public class Fund
     {
         public string FundName { get; set; }
         public string FundNumber { get; set; }
+        public IDictionary CorrelationCoefficients { get; set; }
+        public IList Vector { get; set; }
 
         public Fund(string fundName, string fundNumber)
         {

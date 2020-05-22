15bf50e3d80fab75e2112d467e7d472f957e113f
wshuts <shutswilliam@gmail.com>
2020-05-22 01:31:14 -0500

    JsonIgnore, InitializePriceVector

--- ParameterToolbox/Fund.cs
+++ ParameterToolbox/Fund.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ParameterToolbox
 {
     public class Fund
     {
-        public string FundName { get; set; }
-        public string FundNumber { get; set; }
-        public IDictionary CorrelationCoefficients { get; set; } = new SortedList();
-        public IList<double> PriceVector { get; set; }
-
         public Fund(string fundName, string fundNumber)
         {
             FundName = fundName;
             FundNumber = fundNumber;
+        }
+
+        public string FundName { get; set; }
+        public string FundNumber { get; set; }
+        [JsonIgnore] public IDictionary CorrelationCoefficients { get; set; } = new SortedList();
+        [JsonIgnore] public IList<double> PriceVector { get; set; } = new List<double>();
+        [JsonIgnore] public IDictionary PriceInfo { get; set; } = new SortedList();
+
+        public void InitializePriceVector()
+        {
+            PriceVector.Clear();
+            foreach (string price in PriceInfo.Values) PriceVector.Add(double.Parse(price));
         }
     }
 }

ee89f4982795d22dbb91e2acf4eb57c19e0fef8c
wshuts <shutswilliam@gmail.com>
2020-05-22 01:32:33 -0500

    adding a second fund

--- ParameterToolboxTest/ParametersTests.cs
+++ ParameterToolboxTest/ParametersTests.cs
@@ -31,10 +31,15 @@ namespace ParameterToolboxTest
             var startDate = new DateTime(2020, 01, 01);
             var endDate = new DateTime(2020, 05, 01);
 
-            const string fundName = "Windsor II Fund Adm      ";
-            const string fundNumber = "0573";
-            var fund = new Fund(fundName, fundNumber);
-            var funds = new List<Fund> {fund};
+            const string fund1Name = "Windsor II Fund Adm      ";
+            const string fund1Number = "0573";
+            var fund1 = new Fund(fund1Name, fund1Number);
+            
+            const string fund2Name = "Total Stock Mkt Idx Adm  ";
+            const string fund2Number = "0585";
+            var fund2 = new Fund(fund2Name, fund2Number);
+
+            var funds = new List<Fund> {fund1, fund2};
 
             Parameters = new Parameters {StartDate = startDate, EndDate = endDate, Funds = funds};
         }

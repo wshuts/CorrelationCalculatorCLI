ff2b60ca33a1dd2af2c074cf259d892fcd189e0a
wshuts <shutswilliam@gmail.com>
2020-05-29 01:46:16 -0500

    JsonUtilities

---
+++ ParameterToolbox/JsonUtilities.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace ParameterToolbox
+{
+    public static class JsonUtilities
+    {
+        public static IStream StreamFactory { get; set; } = new StreamFactory();
+        public static IJson JsonFactory { get; set; } = new JsonFactory();
+
+        public static void Serialize(string fullFileName, object source)
+        {
+            using var streamWriter = StreamFactory.CreateStreamWriter(fullFileName);
+            using var jsonWriter = JsonFactory.CreateJsonWriter(streamWriter);
+            var jsonSerializer = JsonFactory.CreateJsonSerializer();
+            jsonSerializer.Serialize(jsonWriter, source);
+        }
+
+        public static T Deserialize<T>(string fullFileName)
+        {
+            using var streamReader = StreamFactory.CreateStreamReader(fullFileName);
+            using var jsonReader = JsonFactory.CreateJsonReader(streamReader);
+            var jsonSerializer = JsonFactory.CreateJsonSerializer();
+            return jsonSerializer.Deserialize<T>(jsonReader);
+        }
+    }
+}

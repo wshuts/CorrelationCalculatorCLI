ed33973f9a57581c296126a58b3ce64ba99d6a94
wshuts <shutswilliam@gmail.com>
2020-06-12 20:15:32 -0500

    test cleaning ParametersFactory

---
+++ ParameterToolbox/ParametersFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParameterToolbox
+{
+    public static class ParametersFactory
+    {
+        public static Parameters CreateParameters()
+        {
+            var startDate = new DateTime(2020, 01, 01);
+            var endDate = new DateTime(2020, 05, 01);
+
+            const string fund1Name = "Windsor II Fund Adm      ";
+            const string fund1Number = "0573";
+            var fund1 = new Fund(fund1Name, fund1Number);
+
+            const string fund2Name = "Total Stock Mkt Idx Adm  ";
+            const string fund2Number = "0585";
+            var fund2 = new Fund(fund2Name, fund2Number);
+
+            var funds = new List<Fund> {fund1, fund2};
+
+            return new Parameters {StartDate = startDate, EndDate = endDate, Funds = funds};
+        }
+    }
+}
--- ParameterToolboxTest/ParametersTests.cs
+++ ParameterToolboxTest/ParametersTests.cs
@@ -12,24 +12,6 @@ namespace ParameterToolboxTest
     {
         public Parameters Parameters { get; private set; }
 
-        private void InitializeParameters()
-        {
-            var startDate = new DateTime(2020, 01, 01);
-            var endDate = new DateTime(2020, 05, 01);
-
-            const string fund1Name = "Windsor II Fund Adm      ";
-            const string fund1Number = "0573";
-            var fund1 = new Fund(fund1Name, fund1Number);
-
-            const string fund2Name = "Total Stock Mkt Idx Adm  ";
-            const string fund2Number = "0585";
-            var fund2 = new Fund(fund2Name, fund2Number);
-
-            var funds = new List<Fund> {fund1, fund2};
-
-            Parameters = new Parameters {StartDate = startDate, EndDate = endDate, Funds = funds};
-        }
-
         [Test]
         public void GivenStreamFactoryMockCanSerializeParameters()
         {
@@ -46,7 +28,7 @@ namespace ParameterToolboxTest
 
             JsonUtilities.StreamFactory = streamFactoryMock.Object;
 
-            InitializeParameters();
+            Parameters = ParametersFactory.CreateParameters();
             Parameters.Serialize();
 
             var parameters = Parameters.Deserialize();

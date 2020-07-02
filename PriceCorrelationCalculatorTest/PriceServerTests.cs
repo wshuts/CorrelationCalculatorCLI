82c43c030ecb43d5772bcd01952ceb4c3ca049ed
wshuts <shutswilliam@gmail.com>
2020-07-02 15:47:24 -0500

    PriceServerTests

---
+++ PriceCorrelationCalculatorTest/PriceServerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace PriceCorrelationCalculatorTest
+{
+    [TestFixture]
+    public class PriceServerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void FooTest()
+        {
+            Assert.True(false);
+        }
+    }
+}

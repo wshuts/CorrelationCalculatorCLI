94e03b03c576b10a2edae5beb7f1ea830fa77c94
wshuts <shutswilliam@gmail.com>
2020-07-03 02:18:36 -0500

    WebCommunicatorMock, CanReadFromWeb

--- PriceCorrelationCalculatorTest/PriceServerTests.cs
+++ PriceCorrelationCalculatorTest/PriceServerTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using Moq;
+using NUnit.Framework;
 using PriceCorrelationCalculator;
 
 namespace PriceCorrelationCalculatorTest
@@ -9,10 +13,31 @@ namespace PriceCorrelationCalculatorTest
         [SetUp]
         public void Setup()
         {
-            PriceServer = new PriceServer();
+            PriceServer = new PriceServer {WebCommunicator = WebCommunicatorMock.Object};
+            WebCommunicatorMockSetup();
+        }
+
+        private readonly WebRequest webRequest = It.IsAny<WebRequest>();
+        private readonly HttpWebResponse httpWebResponse = It.IsAny<HttpWebResponse>();
+
+        private readonly Stream stream = new MemoryStream(Encoding.UTF8.GetBytes("Foo"));
+
+        private void WebCommunicatorMockSetup()
+        {
+            WebCommunicatorMock.Setup(s => s.SetSecurityProtocol());
+            WebCommunicatorMock.Setup(s => s.CreateWebRequest(It.IsAny<string>()))
+                .Returns(webRequest);
+            WebCommunicatorMock.Setup(s => s.InitializeWebRequest(webRequest));
+            WebCommunicatorMock.Setup(s => s.GetWebResponse(webRequest))
+                .Returns(httpWebResponse);
+            WebCommunicatorMock.Setup(s => s.GetResponseStream(httpWebResponse))
+                .Returns(stream);
         }
 
         public PriceServer PriceServer { get; private set; }
+
+        public Mock<IWebCommunicator> WebCommunicatorMock { get; } =
+            new Mock<IWebCommunicator>(MockBehavior.Strict);
 
         private const string ExpectedFundTableQuery =
             "https://personal.vanguard.com/us/funds/tools/pricehistorysearch?Sc=1";
@@ -31,7 +56,13 @@ namespace PriceCorrelationCalculatorTest
             const string requestUri = ExpectedFundTableQuery;
             var responseFromServer = PriceServer.ReadFromWeb(requestUri);
 
-            const string expectedResponseFromServer = "";
+            WebCommunicatorMock.Verify(v => v.SetSecurityProtocol(), Times.Once);
+            WebCommunicatorMock.Verify(v => v.CreateWebRequest(requestUri), Times.Once);
+            WebCommunicatorMock.Verify(v => v.InitializeWebRequest(webRequest), Times.Once);
+            WebCommunicatorMock.Verify(v => v.GetWebResponse(webRequest), Times.Once);
+            WebCommunicatorMock.Verify(v => v.GetResponseStream(httpWebResponse), Times.Once);
+
+            const string expectedResponseFromServer = "Foo";
             Assert.AreEqual(expectedResponseFromServer, responseFromServer);
         }
     }

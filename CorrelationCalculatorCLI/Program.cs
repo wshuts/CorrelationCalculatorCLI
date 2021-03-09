5b234dac94f1d7aeb0c16d90de74565ef702a3cf
wshuts <shutswilliam@gmail.com>
2021-03-09 01:36:59 -0600

    to main, added InputParser, CalculatorFactory

---
+++ CorrelationCalculatorCLI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CorrelationCalculatorCLI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                InputParser.ProcessInputParameters(args);
+                var calculator = CalculatorFactory.CreateCalculator();
+                calculator.CalculateCorrelation();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    internal class CalculatorFactory
+    {
+        public static Calculator CreateCalculator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Calculator
+    {
+        public void CalculateCorrelation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class InputParser
+    {
+        public static void ProcessInputParameters(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
